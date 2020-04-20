8ff62328c5d4c27aff4c4fbfb24e993c77a3a809
Crowdbotics <team@crowdbotics.com>
2020-04-20 16:36:36 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/showcar_supply_16061/MainActivity.java
@@ -0,0 +1,15 @@
+package com.showcar_supply_16061;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "showcar_supply_16061";
+    }
+}
